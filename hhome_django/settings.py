8be2b2ac715969eec7e42de9fc794711d6ff1cdd
Erin Woodworth <elwoodworth+vagrantgit@gmail.com>
2017-05-31 22:57:48 +0000

    Created postgres database.

--- hhome_django/settings.py
+++ hhome_django/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hhome_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hhome_django',
+        'USER': 'hhome_user',
+        'PASSWORD': os.environ['POSTGRES_DB'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
