cd7e02efd7cbe5bbd084a00818e52e60d2154dda
Erin Woodworth <elwoodworth+vagrantgit@gmail.com>
2017-06-02 19:05:24 +0000

    Fixed bug in Chore unit tests. assertIs should have been assertEqal

--- chores/models.py
+++ chores/models.py
@@ -1,15 +1,15 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-from . import field_details
+import field_details
 
 class Chore(models.Model):
     """Chore model"""
     #ID is generated automatically
     name = models.CharField(max_length=63, null=True)
     description = models.CharField(max_length=150, null=True)
-    duration_minutes = models.IntegerField(, null=True)
-    occurance = field_details.OccuranceField(, null=True)
-    days_weekly = field_details.DayOfTheWeekField(, null=True)
+    duration_minutes = models.IntegerField(null=True)
+    occurance = field_details.OccuranceField(null=True)
+    days_weekly = field_details.DayOfTheWeekField(null=True)
     #offers choices of long-written DOW and stores choices as single digits
     #https://stackoverflow.com/questions/5966629/django-days-of-week-representation-in-model
     date_monthly = models.CharField(max_length=2, null=True)
@@ -18,24 +18,24 @@ class Chore(models.Model):
 
     class Meta:
         verbose_name = 'chore'
-        ordering = 'name'
+        ordering = ['name']
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return "<Chore chore_id=%s name=%s>" % (self.chore_id, self.name)
+        return "<Chore chore_id=%s name=%s>" % (self.id, self.name)
 
     def __str__(self):
         return "{}: {}".format(
             self.name,
-            self.description
+            self.description)
 
     def monthly_labor_hours(self):
         """Returns time(min) per month for a given chore"""
-        if self.occurance == 'daily':
-            monthly_hours = self.duration_minutes * 30
-        elif self.occurance == 'weekly':
+        if self.occurance == '1':
+            monthly_hours = self.duration_minutes * int(30)
+        elif self.occurance == '2':
             monthly_hours = len(self.days_weekly.split("|")) * 4 * self.duration_minutes
-        elif self.occurance == 'monthly':
+        elif self.occurance == '3':
             monthly_hours = self.duration_minutes
         return (monthly_hours)    
--- chores/tests.py
+++ chores/tests.py
@@ -5,16 +5,6 @@ from django.test import TestCase
 
 from .models import Chore
 
-    name = models.CharField(max_length=63, null=True)
-    description = models.CharField(max_length=150, null=True)
-    duration_minutes = models.IntegerField(, null=True)
-    occurance = field_details.OccuranceField(, null=True)
-    days_weekly = field_details.DayOfTheWeekField(, null=True)
-    #offers choices of long-written DOW and stores choices as single digits
-    #https://stackoverflow.com/questions/5966629/django-days-of-week-representation-in-model
-    date_monthly = models.CharField(max_length=2, null=True)
-    by_time = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
-
 class ChoreMethodTests(TestCase):
 
     def test_monthly_labor_hours_daily_chores(self):
@@ -24,7 +14,9 @@ class ChoreMethodTests(TestCase):
         """
         daily_chore = Chore(occurance='1', 
                             duration_minutes=30)
-        self.assertIs(daily_chore.monthly_labor_hours(), 900)
+        answer = int(900)
+        question = daily_chore.monthly_labor_hours()
+        self.assertEqual(question, answer)
 
     def test_monthly_labor_hours_weekly_chores(self):
         """
@@ -34,7 +26,9 @@ class ChoreMethodTests(TestCase):
         weekly_chore = Chore(occurance='2', 
                              duration_minutes=30, 
                              days_weekly="0|1|4")
-        self.assertIs(weekly_chore.monthly_labor_hours(), 360)
+        answer = int(360)
+        question = weekly_chore.monthly_labor_hours()
+        self.assertEqual(question, answer)
 
     def test_monthly_labor_hours_monthly_chores(self):
         """
@@ -43,4 +37,6 @@ class ChoreMethodTests(TestCase):
         """
         monthly_chore = Chore(occurance='3', 
                               duration_minutes=30)
-        self.assertIs(monthly_chore.monthly_labor_hours(), 30)
+        answer = int(30)
+        question = monthly_chore.monthly_labor_hours()
+        self.assertEqual(question, answer)
