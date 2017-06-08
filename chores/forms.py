13427a15ab34e1ab7b19aa5de2229e3201753f02
Erin Woodworth <elwoodworth+vagrantgit@gmail.com>
2017-06-08 20:20:11 +0000

    TimeContribution class to forms.py Bourght in logi for a slugclean mixin, but still undecided about using slugs

--- chores/forms.py
+++ chores/forms.py
@@ -8,3 +8,21 @@ class ChoreForm(forms.ModelForm):
     class Meta:
         model = Chore
         fields = '__all__'
+
+# class SlugCleanMixin:
+#     """Mixin class for slug cleaning method."""
+
+#     def clean_slug(self):
+#         new_slug = (
+#             self.cleaned_data['slug'].lower())
+#         if new_slug == 'create':
+#             raise ValidationError(
+#                 'Slug may not be "create".')
+#         return new_slug
+
+
+class TimeContributionForm(
+        SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = TimeContribution
+        fields = '__all__'
